2645378b9ac02ba1e01233ef5079808077235aa6
gabriela <gabysiiu@hotmail.com>
2024-06-18 16:10:04 -0700

    html added, js started code

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiKey = "154fbbfac464b8a0815d5fc0c241c891"
+// Api call
+const apiUrl ="http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=154fbbfac464b8a0815d5fc0c241c891"
+
+// hacer un query selelction del search button 
+
+const searchBtn = document.querySelector('.btn');
+const cityInput = document.querySelector('.searchCity');
+
+//que se haga un event listener"click" se haga la busqueda 
+// de la ciudad que se ingresó en el input
+
+cityInput.addEventListener("input", (event) => {
+ 
+//no se que condición ponerle pero  lo que quiero es que
+//al ponerle el input, la api solicite la info de esa ciudad
+//especifica
+
+
+});
+
+
+
+
+
+
+
+
+// function getApi(){
+//    fetch(apiUrl)
+//    .then(function (response){
+//     return response.json()
+//    })
+
+   
+// }
